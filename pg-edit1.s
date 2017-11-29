0ccd0023a353a36f35f7c67115f915a381249b53
amaanda <mandspereira@gmail.com>
2017-11-29 18:55:24 -0200

    added PG loop

--- pg-edit1.s
+++ pg-edit1.s
@@ -4,10 +4,26 @@
 # Programa em assembly para calcular, a partir de um numero base de entrada, uma progressão geométrica com razão 2, numa sequência de 10 números.
 # Adriele Ramos e Amanda Lucas
 
-		MOV		R0, #0 ;
+# Initial Statements and Registers
+
+		MOV		R0, #0 ; 
 		MOV		R1, #0 ; valor inicial do contador
 		ADD		R9, R0, #2 ; R9 recebe valor de R0 + 2 -  valor de base
 		MOV		R5, #4000 ; memoria a partir da qual valores serão armazenados
 		STR		R9,[R5, #0] ; armazenar valor de base no endereço 4000
 		MOV		R6, #2147483648 ; valor máximo que PG pode alcançar
-		
+
+# PG itself
+
+WHILE
+		CMP		R1, #9		; enquanto não tiver 10 valores na PG
+		BEQ		DONE            ; branch pra sair do WHILE se a flag Zero = 1
+		LSL		R9, R9, #1	; R4 = R4*2 (PG com razão 2)
+		ADD		R8, R9, #0	; registrador R8 recebe resultado da PG
+		ADD		R1, R1, #1	; (contador soma 1)
+		ADD		R5, R5, #4	; deslocamento do endereço de memória em 32 bits
+		STR		R8, [R5, #0]	; resultado da PG armazenado no próximo endereço de memória deslocado
+		TST		R8, R6		; teste/verificar se o resultado da PG é menor que o valor de R6
+		B		WHILE
+DONE
+		LDR		R4, [R5, #0]	; valor final armazenado no registrador RX, X=4.
